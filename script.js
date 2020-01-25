1a0c152e67c05bd4b12f91b9c1bf20e5e421cb77
Breyanna Vass <breyannavass@gmail.com>
2020-01-24 19:37:17 -0500

    making a draw function in the particle class

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ function draw() {}
 class Particle {
   constructor() {
     // Position
-    this.pos = createVector(x, y);
+    this.pos = createVector(random(width), random(height));
+    // Size
+    this.size = 10;
+  }
+
+  draw() {
+    noStroke();
+    fill("rgba(255, 255, 255, 0.5)");
+    circle(this.pos.x, this.pos.y, this.size);
   }
 }
