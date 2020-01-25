905eb337f3f01e70ffde14dd4d35b4e648a354f3
Breyanna Vass <breyannavass@gmail.com>
2020-01-24 19:46:51 -0500

    made some edges for the particle

--- script.js
+++ script.js
@@ -22,13 +22,26 @@ class Particle {
     this.size = 10;
   }
 
+  // update movement
   update() {
     this.pos.add(this.vel);
   }
 
+  // draw particle
   draw() {
     noStroke();
     fill("rgba(255, 255, 255, 0.5)");
     circle(this.pos.x, this.pos.y, this.size);
+  }
+
+  // detect edges
+  edges() {
+    if (this.pos.x < 0 || this.pos.x > width) {
+      this.vel.x *= -1;
+    }
+
+    if (this.pos.y < 0 || this.pos.y > height) {
+      this.vel.y *= -1;
+    }
   }
 }
